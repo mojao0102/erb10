c96226d4cb5c3106f558f888b64c5da3359493ab
mojao0102 <iamos0102@gmail.com>
2026-05-15 17:52:55 +0800

    step 6 : listings get data from DB

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import Paginator
 
 # Create your views here.
 def listings(request):
     listings = Listing.objects.all()
-    context = {"listings": listings}
+    paginator = Paginator(listings, 3)  
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    # print("--- 所有物件的完整資料 ---")
+    # # print(f"At page: {paged_listings.number}")
+    # # for listing in paged_listings:
+    # #     data = {k: v for k, v in listing.__dict__.items() if k != '_state'}
+    # #     print(data)
+    # print("paginator",paginator)
+    # print("page",page)
+    # print("------------------------")
+
+    context = {"listings": paged_listings}
     return render(request, "listings/listings.html", context)
 
 def listing(request, listing_id):
     return render(request, "listings/listing.html")
-
