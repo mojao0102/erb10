2b1080e0bf7860cebe5a7dd303fab83ba30aec6e
mojao0102 <iamos0102@gmail.com>
2026-05-18 11:19:02 +0800

    step 7 : Show listing on details page

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator
 
@@ -8,18 +8,12 @@ def listings(request):
     paginator = Paginator(listings, 3)  
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-
-    # print("--- 所有物件的完整資料 ---")
-    # # print(f"At page: {paged_listings.number}")
-    # # for listing in paged_listings:
-    # #     data = {k: v for k, v in listing.__dict__.items() if k != '_state'}
-    # #     print(data)
-    # print("paginator",paginator)
-    # print("page",page)
-    # print("------------------------")
-
     context = {"listings": paged_listings}
     return render(request, "listings/listings.html", context)
 
 def listing(request, listing_id):
-    return render(request, "listings/listing.html")
+    #Get record by id
+    #listing = Listing.objects.filter(pk=listing_id).first()
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': listing}
+    return render(request, "listings/listing.html", context)
